e65f06221ed27f4331fc3038a80cfc8ac14a717f
Dale Liu <YLiu@lcs.on.ca>
2024-04-09 11:08:36 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yiren LIU on 2024-04-09.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
